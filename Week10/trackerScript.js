d1ade1757e21fe1136d2a5cadcaf58274d52ed17
christme <christme@oregonstate.edu>
2016-05-30 14:16:46 -0700

    updates

--- Week10/trackerScript.js
+++ Week10/trackerScript.js
@@ -30,8 +30,7 @@ var pool = mysql.createPool(
 app.get('/WorkoutTracker',function(req,res,next)
 {
 	var context = {};
-	pool.query("INSERT INTO workouts (`name`, `reps`, `weight`, `date`, `lbs`) VALUES (?)", [req.query.name], [req.query.reps]
-	[req.query.weight], [req.query.date], [req.query.lbs], function(err, result)
+	pool.query("INSERT INTO workouts (`name`) VALUES (?)", [req.query.name], function(err, result)
 	{
 		if(err)
 		{
